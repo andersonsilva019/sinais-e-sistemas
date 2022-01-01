6e5579209d8e0c933012cc1ec81b49902032f3be
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-12-31 22:50:00 -0300

    chore: adicionando comentario no primeiro-sinal

--- primeiro-sinal/impar.py
+++ primeiro-sinal/impar.py
@@ -2,17 +2,19 @@ from math  import *
 from numpy import *
 from pylab import *
 
+# sinal x[n] = 3*n + 5
 def x(n):
     return 3*n + 5
  
 fig = figure(1)
 fig.set_size_inches((8., 8.))
-################################################################################
+
 # Cria as funções a serem plotadas.
 n = arange(-4., 5.)
 xp = 0.5 * (x(n) + x(-n))
 xi = 0.5 * (x(n) - x(-n))
 
+# Plotando o sinal ímpar.
 a =  fig.add_subplot(2, 1, 1)
 a.stem(n, xi, "k-", "ko", "k-")
 a.set_xlim([ -4., 4. ])
